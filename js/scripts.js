251dc4c37d29ee9d8aaa57ef7e4951acb6777bfc
Chan Lee <chanethanlee@gmail.com>
2018-08-13 19:04:02 -0700

    edit bug of submitting button to submitting form, assign div an id, not ul inside div.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-$(document).ready(function (){
-  $("#buttonOne").submit(function(event){
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
+
     var flavors = ["flavor1", "flavor2", "flavor3"]
-    
+
     flavors.forEach(function(flavor){
       var userInput = $("input#" + flavor).val();
-      $("#output ul").append("<li>" + userInput + "<li>");
+      $("#output ul").append("<li>" + userInput + "</li>");
     });
   });
 });
