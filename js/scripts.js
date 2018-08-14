04e8b9fff3635edfb2f2bb0b472c7f2959671964
Chan Lee <chanethanlee@gmail.com>
2018-08-13 18:44:31 -0700

    make button and add submit functionality to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function (){
-
-  var flavors = ["flavor1", "flavor2", "flavor3"]
-  flavors.forEach(function(flavor){
-    var userInput = $("input#" + flavor).val();
-    $("#output ul").append("<li>" + userInput + "<li>");
+  $("#buttonOne").submit(function(event){
+    event.preventDefault();
+    var flavors = ["flavor1", "flavor2", "flavor3"]
+    
+    flavors.forEach(function(flavor){
+      var userInput = $("input#" + flavor).val();
+      $("#output ul").append("<li>" + userInput + "<li>");
+    });
   });
-
 });
